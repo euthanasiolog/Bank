cb5d745634bcd20e93813fa4fdb216791d335177
plizzzhealme <plizzz.healme@gmail.com>
2016-10-23 01:07:43 +0300

    Update Bank.java

--- src/Bank.java
+++ src/Bank.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
-
-/**
- * Created by Piatrok on 19.10.16.
- */
 public class Bank
-{   
+{
     /**
-    *Делает трасфер
-    * amount - сумма
-    * from - to  откуда - куда
-    */
-    public void transfer (int amount, Account from, Account to)
+     * Делает трансфер денег
+     * @param amount сумма
+     * @param from откуда
+     * @param to куда
+     */
+    public static void transfer (int amount, Account from, Account to)
     {
         //тут пиши код перевода
     }
-    
+
     public static void main (String[]args)
 
     {
@@ -29,25 +26,23 @@ public class Bank
         System.out.println("set money on account2");
         account2.setMoney(scanner.nextInt());
 
-        System.out.println("Money on account1 = " + account1.getMomey());
-        System.out.println("Money on account2 = " + account2.getMomey());
+        System.out.println("Money on account1 = " + account1.getMoney());
+        System.out.println("Money on account2 = " + account2.getMoney());
 
+        int transfer;
         // перевод вводимой суммы с аккаунта1 на аккаунт2
         System.out.println("How much money transfer from account1 to account2? ");
-        int transfer1 = scanner.nextInt();
-        //и заменяешь методом те строки, во втором случае так же
-        transfer (transfer1, account1, account2);
+        transfer = scanner.nextInt();
 
-        System.out.println("Money on account1 = " + account1.getMomey());
-        System.out.println("Money on account2 = " + account2.getMomey());
+        System.out.println("Money on account1 = " + account1.getMoney());
+        System.out.println("Money on account2 = " + account2.getMoney());
 
         // перевод вводимой суммы с аккаунта2 на аккаунт1
         System.out.println("How much money transfer from account2 to account1? ");
-        int transfer2 = scanner.nextInt();
-        account1.Transfer(transfer2);
-        account2.Transfer(-transfer2);
+        transfer = scanner.nextInt();
 
-        System.out.println("Money on account1 = " + account1.getMomey());
-        System.out.println("Money on account2 = " + account2.getMomey());
-       }
+
+        System.out.println("Money on account1 = " + account1.getMoney());
+        System.out.println("Money on account2 = " + account2.getMoney());
+    }
 }
